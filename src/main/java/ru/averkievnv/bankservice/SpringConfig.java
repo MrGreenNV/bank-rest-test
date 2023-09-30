59effa4eda692e9d64adf88d442c2e15397334b4
MrGreenNV <averkievnv@gmail.com>
2023-09-30 18:28:10 +0300

    add SpringConfig.java Добавлена конфигурация приложения

---
+++ src/main/java/ru/averkievnv/bankservice/SpringConfig.java
@@ -0,0 +1,33 @@
+package ru.averkievnv.bankservice;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+/**
+ * @author mrGreenNV
+ */
+@Configuration
+@EnableWebMvc
+public class SpringConfig {
+
+    /**
+     * Создает Bean для преобразования DTO к модели и наоборот.
+     * @return объект ModelMapper.
+     */
+    @Bean
+    public ModelMapper getModelMapper() {
+        return new ModelMapper();
+    }
+
+    /**
+     * Создает Bean BCryptPasswordEncoder для кодирования информации.
+     * @return новый объект BCryptPasswordEncoder.
+     */
+    @Bean
+    public BCryptPasswordEncoder getBCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
